d15207b538982154c45db8a56c73d1912e936c21
RJ McCarthy <128163043+mccarthyrj@users.noreply.github.com>
2024-10-17 12:19:59 -0400

    Update ParameterContainer.jsx
    Removes erroneous Slider import.

--- client/components/ParameterContainer.jsx
+++ client/components/ParameterContainer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
